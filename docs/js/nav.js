8145ba662fb90986bb3f59def9ecce0116e88389
Romain Michon <rmnmichon@gmail.com>
2022-04-28 14:22:31 +0200

    Adding keynotes

--- docs/js/nav.js
+++ docs/js/nav.js
@@ -5,8 +5,9 @@ document.getElementById("nav-menu").innerHTML =
     '<div class="menu-links">'+
     '<ul>'+
     '<li><a href="index.html">Home</a></li>'+
-'<li><a href="program.html">Program</a></li>'+
-'<li><a href="conferences.html">Confs</a></li>'+
+    '<li><a href="program.html">Program</a></li>'+
+    '<li><a href="keynotes.html">Keynotes</a></li>'+
+    '<li><a href="conferences.html">Confs</a></li>'+
     '<li><a href="festival.html">Festival</a></li>'+
     '<li><a href="faites.html">Faites du Son!</a></li>'+
     '<li><a href="school.html">Sum. School</a></li>'+
